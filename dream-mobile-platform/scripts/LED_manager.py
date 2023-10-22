dc30d6b92f33d3d78b19463ac05120be3bf48355
ricocoyeah <rjia@diligentrobots.com>
2023-10-21 22:33:36 -0500

    led manager imports

--- dream-mobile-platform/scripts/LED_manager.py
+++ dream-mobile-platform/scripts/LED_manager.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-import RPi.GPIO as GPIO
 import time
-import socket
-import os
-import pigpio
+from pwm_servo_utils import general_init, motors_init, servos_init, LED_init, motors_cleanup, general_cleanup, servos_cleanup
+from pwm_servo_utils import forward, backward, brake, spin_left, spin_right
+from pwm_servo_utils import LED_magenta, LED_blue, LED_off
+from pwm_servo_utils import vertical_servo_control, horizontal_servo_control
+from pwm_servo_utils import camera_init
