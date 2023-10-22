da6d89feefaa9b32ae6538cc63bcbe3f589c8e93
ricojia <ricoruotongjia@gmail.com>
2023-10-22 13:50:20 -0500

    LED toggle is working

--- src/dream_mobile_platform/scripts/pwm_led_manager.py
+++ src/dream_mobile_platform/scripts/pwm_led_manager.py
@@ -3,11 +3,8 @@
 import rospy
 from dream_mobile_platform.srv import ToggleLED
 
-from pwm_servo_utils import general_init, motors_init, servos_init, LED_init, motors_cleanup, used_pins_cleanup, servos_cleanup
-from pwm_servo_utils import forward, backward, brake, spin_left, spin_right
-from pwm_servo_utils import LED_magenta, LED_blue, LED_off
-from pwm_servo_utils import vertical_servo_control, horizontal_servo_control
-from pwm_servo_utils import camera_init
+from pwm_servo_utils import general_init, servos_init, LED_init, used_pins_cleanup, servos_cleanup
+from pwm_servo_utils import set_LED_state
 
 LED_R = 22
 LED_G = 27
@@ -17,14 +14,14 @@ class PWM_LED_Manager:
     def __init__(self) -> None:
         general_init()
         LED_init()
-        motors_init()
         self.led_server = rospy.Service(rospy.get_param("/PARAM/LED_SERVICE", '/toggle_led'), ToggleLED, self.toggle_led)
 
     def cleanup(self):
         used_pins_cleanup()
+        print(f'Pins have been cleaned up')
 
     def toggle_led(self, req):
-        pass
+        return set_LED_state(req.state)
 
 if __name__ == '__main__':
     rospy.init_node('pwm_led_manager')
