cec942407d98eeb60303b8c1951a8f3a5a9dea8b
ricojia <ricoruotongjia@gmail.com>
2023-10-28 12:47:22 -0500

    In progress: motor encoder

--- src/dream_mobile_platform/scripts/motor_encoder.py
+++ src/dream_mobile_platform/scripts/motor_encoder.py
@@ -1,62 +1,86 @@
 import RPi.GPIO as GPIO
 import numpy as np
+import enum
+from pwm_servo_utils import general_init
+import logging
 
+LEFT_PHASE_A = 7
+RIGHT_PHASE_A = 6
+LEFT_PHASE_B = 12
+RIGHT_PHASE_B = 17
+# encoder ticks, according to the datasheet, it's 374
+RIGHT_ENCODER_PULSE_PER_REV = 362 #361.8
+# encoder ticks, according to the datasheet, it's 374
+LEFT_ENCODER_PULSE_PER_REV = 367 #367.39
 
-Left_Phase_A = 7      #(left motor phase A)
-Right_Phase_A = 6     #(right motor phase A)
-Left_Phase_B = 12    #(left motor phase B)
-Right_Phase_B = 17 #(right phaseB)
-RIGHT_ENCODER_RESOLUTION = 362 #361.8  #(encoder ticks, according to the datasheet, it's 374)
-LEFT_ENCODER_RESOLUTION = 367 #367.39  #(encoder ticks, according to the datasheet, it's 374)
+class MotorMode(enum.Enum):
+    LEFT = enum.auto()
+    RIGHT = enum.auto()
 
-def get_left_encoder_vals():
-    """ Return a list of left encoder pin readings"""
-    left_A = GPIO.input(Left_Phase_A)
-    left_B  = GPIO.input(Left_Phase_B)
-    return [left_A, left_B]
-
-def get_right_encoder_vals():
-    """ Return a list of right encoder pin readings"""
-    right_A = GPIO.input(Right_Phase_A)
-    right_B = GPIO.input(Right_Phase_B)
-    return [right_A, right_B]
-
-class WheelEncodersPublisher:
+class WheelEncodersReader:
+    """
+    Right hand rule for motor count is followed. I.e., CCW is positive.
+    """
     def __init__(self):
+        general_init()
         self._encoder_measurement_init()
-
-    def _encoder_measurement_init(self):
-        GPIO.setup(Left_Phase_A,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(Right_Phase_A,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(Left_Phase_B,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(Right_Phase_B,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        """Initialize interrupt-driven encoder callbacks"""
-        GPIO.add_event_detect(Right_Phase_A, GPIO.RISING, self.encoder_counter)
-        GPIO.add_event_detect(Left_Phase_B, GPIO.RISING, self.encoder_counter)  #Note we cannot use Left_Phase_A because of other functions on this pin. This is why we need two seaparate encoder counters.
         self.right_count = 0
         self.last_right_count = 0
         self.left_count = 0
         self.last_left_count = 0
 
-    def encoder_counter(self, pin):
-        if (pin == Right_Phase_A):
-            vals = get_right_encoder_vals()
-            # self.right_count += (self.encounter_variation_right(A_val=vals[0], B_val=vals[1]))
-            # # Angle wrapping: half angle
-            # if np.abs(self.right_count) > 0.5 * RIGHT_ENCODER_RESOLUTION:
-            #     self.right_count = np.abs(self.right_count) * (np.abs(self.right_count) - RIGHT_ENCODER_RESOLUTION)
+    def _encoder_measurement_init(self):
+        """Initialize interrupt-driven encoder callbacks"""
+        GPIO.setup(LEFT_PHASE_A,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(RIGHT_PHASE_A,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(LEFT_PHASE_B,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(RIGHT_PHASE_B,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(RIGHT_PHASE_A, GPIO.RISING, self._rising_edge_encoder_counter)
+        # Note we cannot use LEFT_PHASE_A because of other functions on this pin. 
+        # This is why we need two seaparate encoder counters.
+        GPIO.add_event_detect(LEFT_PHASE_B, GPIO.RISING, self._rising_edge_encoder_counter)
+        
+    def _get_encoder_phase_values(self, motor_mode: MotorMode):
+        """ Return a list of left encoder pin readings"""
+        if motor_mode == MotorMode.LEFT:
+            return [GPIO.input(LEFT_PHASE_A), GPIO.input(LEFT_PHASE_A)]
+        elif motor_mode == MotorMode.RIGHT:
+            return [GPIO.input(RIGHT_PHASE_A), GPIO.input(RIGHT_PHASE_B)]
         else:
-            vals = get_left_encoder_vals()
-            # self.left_count += (self.encounter_variation_left(A_val=vals[0], B_val=vals[1]))
-            # # Angle wrapping: half angle
-            # if np.abs(self.left_count) > 0.5 * LEFT_ENCODER_RESOLUTION:
-            #     self.left_count = np.abs(self.left_count) * (np.abs(self.left_count) - LEFT_ENCODER_RESOLUTION)
-        return vals
+            print(f"Please pass in valid MotorMode values: {MotorMode._member_names_}")
+            return []
+
+    def _rising_edge_encoder_counter(self, pin: int):
+        """
+        Pin-agnostic encoder counter that updates the corresponding pulse counter. 
+        that is, no matter which pins' rising edge are detected,
+        We only care if it's phase A or B. Then, we angle-wrap this value.
+        """
+        # Get phase values and which count based on left or right
+        motor_mode = MotorMode.RIGHT if pin in [RIGHT_PHASE_A, RIGHT_PHASE_B] else MotorMode.LEFT
+        phase_a_val, phase_b_val = self._get_encoder_phase_values(motor_mode)
+        
+        # Update increment based on phase A or B.TODO: to find CCW .
+        if pin in [RIGHT_PHASE_A, LEFT_PHASE_A]:
+            increment = 1 if phase_a_val == phase_b_val else -1
+        else:
+            increment = -1 if phase_a_val == phase_b_val else 1
+        if motor_mode == MotorMode.LEFT:
+            self.left_count += increment 
+            # angle wrap -> 1 rev
+        else:
+            self.right_count += increment
+        # self.left_count += (self.encounter_variation_left(A_val=vals[0], B_val=vals[1]))
+        # # Angle wrapping: half angle
+        # if np.abs(self.left_count) > 0.5 * LEFT_ENCODER_RESOLUTION:
+        #     self.left_count = np.abs(self.left_count) * (np.abs(self.left_count) - LEFT_ENCODER_RESOLUTION)
 
 if __name__ == "__main__":
     import time
-    wheel_encoders_publisher = WheelEncodersPublisher()
+    wheel_encoders_publisher = WheelEncodersReader()
     while True:
-        right_val = wheel_encoders_publisher.encoder_counter(Right_Phase_A)
-        left_val = wheel_encoders_publisher.encoder_counter(Left_Phase_B)
+        # right_val = wheel_encoders_publisher._rising_edge_encoder_counter(RIGHT_PHASE_A)
+        # left_val = wheel_encoders_publisher._rising_edge_encoder_counter(LEFT_PHASE_B)
+        # #TODO Remember to remove
+        # print(f'Rico: left {left_val}, right: {right_val}')
         time.sleep(0.5)
--- src/dream_mobile_platform/scripts/test_motors.py
+++ src/dream_mobile_platform/scripts/test_motors.py
@@ -1,10 +1,11 @@
 from pwm_servo_utils import *
+from motor_encoder import WheelEncodersReader
 import time
 
 general_init()
 motors_init()
 
-
+wer = WheelEncodersReader()
 start = time.time()
 while True:
     try:
@@ -19,6 +20,8 @@ full_rev = int(input("how many full revolutions?"))
 partial_rev = int(input("how many partial revolutions?"))
 total_rev = full_rev + partial_rev/14.0
 print(f"total revs: {total_rev}") 
+
+print(f"left right encoder value: ${wer.right_count, wer.left_count}")
 
 used_pins_cleanup() 
 
