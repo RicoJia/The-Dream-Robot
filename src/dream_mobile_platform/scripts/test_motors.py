996e9ef7aae519d6a495b52fb93bd832613a3f41
ricocoyeah <rjia@diligentrobots.com>
2023-10-28 19:02:52 -0500

    gpio lib test code

--- src/dream_mobile_platform/scripts/test_motors.py
+++ src/dream_mobile_platform/scripts/test_motors.py
@@ -1,15 +1,18 @@
 from pwm_servo_utils import *
-from motor_encoder import WheelEncodersReader
+from motor_encoder import WheelEncodersReader, SimpleTestWheelEncoder
 import time
 
 general_init()
 motors_init()
 
-wer = WheelEncodersReader()
+# wer = WheelEncodersReader()
+stwe = SimpleTestWheelEncoder()
 start = time.time()
 while True:
     try:
         forward(5, 5)
+        #TODO Remember to remove
+        print(f'Angle: {stwe.get_angle()}')
     except KeyboardInterrupt:
         motors_cleanup()
         break
@@ -21,7 +24,7 @@ partial_rev = float(input("how many partial revolutions?"))
 total_rev = full_rev + partial_rev/14.0
 print(f"total revs: {total_rev}") 
 
-print(f"left right encoder value: ${wer.right_count/total_rev, wer.left_count/total_rev}")
+# print(f"left right encoder value: ${wer.right_count/total_rev, wer.left_count/total_rev}")
 
 used_pins_cleanup() 
 
