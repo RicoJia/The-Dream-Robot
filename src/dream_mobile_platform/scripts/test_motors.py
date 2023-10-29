5844cfdb26790b28e7539b0fd2429c2a3b1a2e2d
ricojia <ricoruotongjia@gmail.com>
2023-10-29 13:09:09 -0500

    pigpio works

--- src/dream_mobile_platform/scripts/test_motors.py
+++ src/dream_mobile_platform/scripts/test_motors.py
@@ -1,30 +1,35 @@
 from pwm_servo_utils import *
 # from motor_encoder import WheelEncodersReader
-from motor_encoder import SimpleTestWheelEncoder
+# from motor_encoder import SimpleTestWheelEncoder
+from motor_encoder import PigpioDecoder
 import time
 
 general_init()
 motors_init()
 
 # wer = WheelEncodersReader()
-wer = SimpleTestWheelEncoder()
+# wer = SimpleTestWheelEncoder()
+wer = PigpioDecoder()
 start = time.time()
 started = False
 while True:
     try:
-        forward(1, 1)
         # if time.time() - start > 1 and not started:
         #     start_val = wer.get_angle()
         #     started = True
         #     print("started")
-        # if time.time() - start > 4:
-        #     end_val = wer.get_angle()
-        #     print("in 2s, number of ticks: ", end_val - start_val)
-        #     break
+        # if time.time() - start > 23:
+        #     motors_cleanup()
+        #     # pass
+        #     # end_val = wer.get_angle()
+        #     # print("in 2s, number of ticks: ", end_val - start_val)
+        #     # break
+        # else:
         print(wer.get_angle())
-        if wer.get_angle() >= 352 * 3:
+        if wer.get_angle() >= 1496 * 120:
             motors_cleanup()
-            break
+        else:
+            forward(10, 10)
 
     except KeyboardInterrupt:
         motors_cleanup()
