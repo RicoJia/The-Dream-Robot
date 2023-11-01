713af38b141f254c0b84b00b22fde2fcb7506963
ricojia <ricoruotongjia@gmail.com>
2023-11-01 00:24:55 -0500

    test result is out

--- src/dream_mobile_platform/tests/compare_shm_ros_pubsub.py
+++ src/dream_mobile_platform/tests/compare_shm_ros_pubsub.py
@@ -24,32 +24,17 @@ class ShmSub:
         th.start()
     def _run(self):
         while not rospy.is_shutdown():
-            self.sem.acquire()
+            # self.sem.acquire()
             try:
                 packed_data = self.mmap[:4]
                 msg = struct.unpack('f', packed_data)[0]
                 self.cb(msg)
             finally:
-                self.sem.release()
+                # self.sem.release()
+                pass
     def cleanup(self):
         self.sem.unlink()
         self.shm.unlink()
-   
-def check_shm_pub_sub(i):
-    def cb(num):
-        #TODO Remember to remove
-        print(f'Rico: {num}')
-    sub = ShmSub(f'shm_{i}', cb)
-    pub = ShmPub(f'shm_{i}')
-    while not rospy.is_shutdown():
-        pub.publish(i)
-        rospy.sleep(0.02)
-    sub.cleanup()
-    pub.cleanup() 
-
-#####################################################
-# ROS pub/sub
-#####################################################
 
 class ShmPub:
     def __init__(self, name):
@@ -68,18 +53,36 @@ class ShmPub:
         packed_data = struct.pack('f', msg)
         if len(packed_data) > SHM_SIZE:
             raise ValueError('Message too large')
-        self.sem.acquire()
+        # self.sem.acquire()
         try:
             self.mmap[:len(packed_data)] = packed_data
         finally:
-            self.sem.release()
+            # self.sem.release()
+            pass
     def cleanup(self):
         self.mmap.close()
         self.sem.unlink()
-        self.shm.unlink()
+        self.shm.unlink() 
+
+def check_shm_pub_sub(i):
+    # on rpi, 80? tmux 45; without semaphores, 80%
+    def cb(num):
+        #TODO Remember to remove
+        print(f'Rico: {num}')
+    sub = ShmSub(f'shm_{i}', cb)
+    pub = ShmPub(f'shm_{i}')
+    while not rospy.is_shutdown():
+        pub.publish(i)
+        rospy.sleep(0.02)
+    sub.cleanup()
+    pub.cleanup() 
+
+#####################################################
+# ROS pub/sub
+#####################################################
         
 def check_pub_sub(i):
-    # 100%
+    # 40%, 30% for tmux
     def cb(msg):
         #TODO Remember to remove
         print(f'Rico: {msg.data}')
@@ -90,10 +93,16 @@ def check_pub_sub(i):
         rospy.sleep(0.02)
     
 if __name__ == '__main__':
-    rospy.init_node('compare_shm_ros_pubsub', anonymous=True)
+    # rospy.init_node('compare_shm_ros_pubsub', anonymous=True)
     threads = [
     Thread(target=check_shm_pub_sub, args=(i,), daemon=True) for i in range(10)]
     # Thread(target=check_pub_sub, args=(i,), daemon=True) for i in range(10)]
     [t.start() for t in threads]
-    rospy.spin()
+    # rospy.spin()
+    while True:
+        try:
+            rospy.sleep(1)
+        except KeyboardInterrupt:
+            break
+        
     
