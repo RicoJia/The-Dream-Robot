8c63b8bd14578c1b84187a3f5ed162cedb145adf
ricocoyeah <rjia@diligentrobots.com>
2023-11-26 17:32:42 -0600

    docker file update, and documentation

--- src/dream_mobile_platform/scripts/test_motors.py
+++ src/dream_mobile_platform/scripts/test_motors.py
@@ -9,9 +9,7 @@
 from collections import deque
 import time
 from dream_mobile_platform.motor_controller import MotorControlBench, PIDParams
-from simple_robotics_python_utils.pubsub.shared_memory_pub_sub import (
-    SharedMemoryPub
-)
+from simple_robotics_python_utils.pubsub.shared_memory_pub_sub import SharedMemoryPub
 import rospy
 import typing
 import random
@@ -23,9 +21,13 @@ KP_MAX = 1
 KI_MAX = 0.1
 KD_MAX = 0.3
 # TODO: before we run this, we need to see if these velocities make sense
-TEST_SEQUENCE = ((0.1, 5), (0.6, 5), ) # (0.2, 5), (0.3, 5), (0.1, 5), (0.0, 5)
+TEST_SEQUENCE = (
+    (0.1, 5),
+    (0.6, 5),
+)  # (0.2, 5), (0.3, 5), (0.1, 5), (0.0, 5)
 NUM_GENERATIONS = 1
 CHILDREN_NUM = 1
+
 
 def generate_initial_children() -> typing.List[typing.Tuple[PIDParams, PIDParams]]:
     # Generate random numbers for P, I, D
@@ -42,88 +44,104 @@ def generate_initial_children() -> typing.List[typing.Tuple[PIDParams, PIDParams
                     random.uniform(0, KP_MAX),
                     random.uniform(0, KI_MAX),
                     random.uniform(0, KD_MAX),
-                )
-                
+                ),
             )
         )
     return initial_children
 
+
 def select_fittest_population(population):
     pass
+
 
 def reproduce(parents):
     # cross over, then mutate
     pass
 
+
 def score_speed_trajectory(test_data_length_stamps, test_data):
     pass
 
-def start_test_and_record(left_and_right_pid_params: typing.Tuple[PIDParams, PIDParams]):
+
+def start_test_and_record(
+    left_and_right_pid_params: typing.Tuple[PIDParams, PIDParams]
+):
     """
     - test_output = []; will be nice to have (0.5 - [])...
-    - controller has a subscriber -> encoder; sub -> commanded wheel vel; publisher -> speed pub; 
+    - controller has a subscriber -> encoder; sub -> commanded wheel vel; publisher -> speed pub;
         - ideal case: when timeout -> records length of output list -> keep stepping end of the test: sum(abs(setpoint - v[k]))/len(list)
-        - currently: 
-            1. controller automatically pubs 0 if the subscribers are down. 
+        - currently:
+            1. controller automatically pubs 0 if the subscribers are down.
             2. Otherwise, it will publish velocity explictly.
     - Running Env: dream_byobu without motor_controller launched.
     """
+
     def test_worker(test_data, test_data_length_stamps):
         # launching a pub because it has to be within the same process
         commanded_wheel_vel_pub = SharedMemoryPub(
             topic=rospy.get_param("/SHM_TOPIC/COMMANDED_WHEEL_VELOCITY"),
-            data_type = float,
-            arr_size = 2,
-            debug = False 
+            data_type=float,
+            arr_size=2,
+            debug=False,
         )
         mcb = MotorControlBench(*left_and_right_pid_params)
         # Publishing for 2 motors
         time.sleep(0.1)
         for v_set_point, test_time in TEST_SEQUENCE:
-            #TODO Remember to remove
-            print(f'Rico: set_point: {v_set_point}')
+            # TODO Remember to remove
+            print(f"Rico: set_point: {v_set_point}")
             start_time = time.perf_counter()
             commanded_wheel_vel_pub.publish([v_set_point, v_set_point])
             while time.perf_counter() - start_time < test_time:
-                actual_speeds = mcb.step()
+                # place the speed reading before stepping, because it takes
+                # time for the step to take effect.
+                actual_speeds = mcb.get_actual_speeds()
+                mcb.step()
                 test_data.append(actual_speeds)
             test_data_length_stamps.append(len(test_data))
             commanded_wheel_vel_pub.publish([0.0, 0.0])
-            
+
     with Manager() as manager:
         test_data = manager.list()
         test_data_length_stamps = manager.list()
-        print(f'Start testing child: {left_and_right_pid_params}')
-        test_proc = Process(target=test_worker, args = (test_data, test_data_length_stamps))
+        print(f"Start testing child: {left_and_right_pid_params}")
+        test_proc = Process(
+            target=test_worker, args=(test_data, test_data_length_stamps)
+        )
         test_proc.start()
         test_proc.join()
-        #TODO Remember to remove
-        print(f'Rico: test data: {test_data}')
-        score = score_speed_trajectory(test_data_length_stamps, test_data) 
+        # TODO Remember to remove
+        print(f"Rico: test data: {test_data}")
+        score = score_speed_trajectory(test_data_length_stamps, test_data)
     return score
-        
-        
+
+
 class GeneticAlgorithmPIDTuner:
-    __slots__ = ('population', 'scores', 'commanded_wheel_vel_pub')
+    __slots__ = ("population", "scores", "commanded_wheel_vel_pub")
+
     def __init__(self):
         self._load_population_data()
-        
+
     def run(self):
-        for _ in range(NUM_GENERATIONS): 
-            if self.population: 
+        for _ in range(NUM_GENERATIONS):
+            if self.population:
                 fittest_population = select_fittest_population(self.population)
-                children: typing.List[typing.Tuple[PIDParams, PIDParams]] = reproduce(fittest_population)
+                children: typing.List[typing.Tuple[PIDParams, PIDParams]] = reproduce(
+                    fittest_population
+                )
             else:
-                children: typing.List[typing.Tuple[PIDParams, PIDParams]] = generate_initial_children() 
-            for child in children: 
-                score = start_test_and_record(child) 
+                children: typing.List[
+                    typing.Tuple[PIDParams, PIDParams]
+                ] = generate_initial_children()
+            for child in children:
+                score = start_test_and_record(child)
                 self.record_score_and_update_population(score, child)
 
     def record_score_and_update_population(self, score, pid):
         # record the score in PERFORMANCE_FILE
         # add the child and score to population
         pass
-        
+
     def _load_population_data(self):
         # TODO : to load params properly
         self.population = {}
@@ -140,7 +158,7 @@ Design decision on lanching a separate process for a single test bench
         - Need: two lists: test data, and timestamp. And that requires multiprocessing.manager, and sharedlist. 
         - Implementation is not too complicated, and it's forward compatible.
 """
-if __name__ == '__main__':
+if __name__ == "__main__":
     # DO NOT LAUNCH motor_controller.py. Setting node name to test_motors
     rospy.init_node("test_motors")
     ga_pid_tuner = GeneticAlgorithmPIDTuner()
--- src/dream_mobile_platform/src/dream_mobile_platform/motor_controller.py
+++ src/dream_mobile_platform/src/dream_mobile_platform/motor_controller.py
@@ -2,51 +2,56 @@
 from simple_robotics_python_utils.pubsub.pub_sub_utils import Rate
 from simple_robotics_python_utils.pubsub.shared_memory_pub_sub import (
     SharedMemorySub,
-    SharedMemoryPub
+    SharedMemoryPub,
 )
 import rospy
 from collections import deque, namedtuple
 from typing import Tuple, Deque
 import numpy as np
-# from numba import njit
+from numba import njit
 
 PIDParams = namedtuple("PIDParams", ["kp", "ki", "kd"])
 
-LEFT=0
-RIGHT=1
+LEFT = 0
+RIGHT = 1
 NUM_ERRORS = 2
 
-# we want to save params: P, I, D.
-# Generates a list of: pwm, speed 
+
 class IncrementalPIDController:
+    """Controller for both motors using incremental PID controller"""
+
     def __init__(self, left_params: PIDParams, right_params: PIDParams):
-        self.kp = np.array((left_params.kp, right_params.kp)) 
+        self.kp = np.array((left_params.kp, right_params.kp))
         self.ki = np.array((left_params.ki, right_params.ki))
         self.kd = np.array((left_params.kd, right_params.kd))
         self.stop_getting_wheel_vel_updates()
         self.stop_taking_commands()
 
-   ######################################### Wheel Vel Update Functions #########################################
+    ######################################### Wheel Vel Update Functions #########################################
     def store_speed(self, speed: Tuple[float, float]) -> None:
-        """This is a callback for subscribers
+        """Callback for hearing actual speeds
 
         Args:
             speed (Tuple[float, float]): motor speed
         """
         self.motor_speeds = np.asarray(speed)
-    
+
     def start_getting_wheel_vel_updates(self):
+        """Set flag to True. Relevant params have been reset already"""
         # [e[k-1], e[k-2]]
         self.getting_speed_updates = True
-        
+
     def stop_getting_wheel_vel_updates(self):
+        """Function that resets current speeds and errors and flag."""
         self.getting_speed_updates = False
         self.motor_speeds = np.array((0.0, 0.0))
-        self.errors: Deque[np.ndarray] = deque([np.zeros(2) for _ in range(NUM_ERRORS)], maxlen=NUM_ERRORS)
-        
-   ######################################### Commanded Wheel Vel Functions #########################################
+        self.errors: Deque[np.ndarray] = deque(
+            [np.zeros(2) for _ in range(NUM_ERRORS)], maxlen=NUM_ERRORS
+        )
+
+    ######################################### Commanded Wheel Vel Functions #########################################
     def store_commanded_speed(self, speed: Tuple[float, float]) -> None:
-        """Callback for commanded speed
+        """Callback for hearing commanded speed
 
         Args:
             speed (Tuple[float, float]): motor speed
@@ -54,52 +59,95 @@ class IncrementalPIDController:
         self.desired_speeds = np.asarray(speed)
 
     def start_taking_commands(self):
+        """Set flag to True. Relevant params have been reset already"""
         self.getting_commanded_wheel_vel = True
-    
+
     def stop_taking_commands(self):
-        self.desired_speeds = np.array((0.0, 0.0)) 
+        """Function that resets current speeds and pwm and flag."""
+        self.desired_speeds = np.array((0.0, 0.0))
         self.last_pwm: np.ndarray = np.zeros(2)
         self.getting_commanded_wheel_vel = False
-        
+
     def get_pwms(self) -> Tuple[float, float]:
+        """High level function to calculate current PWM.
+
+        Returns:
+            Tuple[float, float]: PWM for both motors
+        """
         if self.getting_speed_updates and self.getting_commanded_wheel_vel:
             # e[k] = desired_speeds[k] - motor_speeds[k]
             e = np.asarray(self.desired_speeds) - self.motor_speeds
-            current_pwm = IncrementalPIDController.calc_pid(e, self.errors, self.kp, self.ki, self.kd, self.last_pwm)
+            current_pwm = IncrementalPIDController.calc_pid(
+                e, self.errors, self.kp, self.ki, self.kd, self.last_pwm
+            )
             # TODO
-            print(f'Rico: motor speed: {self.motor_speeds}, pwm: {current_pwm}')
+            print(f"Rico: motor speed: {self.motor_speeds}, pwm: {current_pwm}")
             self.errors.appendleft(e)
-            self.last_pwm = current_pwm 
+            self.last_pwm = current_pwm
             return tuple(current_pwm)
         else:
             return (0.0, 0.0)
-        
+
     @staticmethod
-    # @njitu
-    def calc_pid(e, past_errors, kp, ki, kd, last_pwm) -> np.ndarray:
+    @njit
+    def calc_pid(
+        e: np.ndarray,
+        past_errors: Deque[np.ndarray],
+        kp: np.ndarray,
+        ki: np.ndarray,
+        kd: np.ndarray,
+        last_pwm: np.ndarray,
+    ) -> np.ndarray:
+        """Util function to calculate the current pwm value.
+
+        Args:
+            e (np.ndarray): set_point - current_value
+            past_errors (np.ndarray): e[k-1], e[k-2]
+            kp (np.ndarray): 2-array for both motors
+            ki (np.ndarray): 2-array for both motors
+            kd (np.ndarray): 2-array for both motors
+            last_pwm (np.ndarray): last pwm signal
+
+        Returns:
+            np.ndarray: current pwm
+        """
         # u[k] = kp * (e[k] - e[k-1]) + ki * e[k] + kd * (e[k] - 2 * e[k-1] + e[k-2])
-        u = kp * (e - past_errors[0]) + ki * e + kd * (e - 2 * past_errors[0] + past_errors[1])
+        u = (
+            kp * (e - past_errors[0])
+            + ki * e
+            + kd * (e - 2 * past_errors[0] + past_errors[1])
+        )
         current_pwm = u + last_pwm
         current_pwm = np.clip(current_pwm, 0.0, 1.0)
-        #TODO Remember to remove
-        print(f'Rico: e: {e}, u: {u}')
+        # TODO Remember to remove
+        print(f"Rico: e: {e}, u: {u}")
         return current_pwm
 
     def get_actual_speeds(self) -> Tuple[float, float]:
+        """Small function to get the two motor speeds"""
         return tuple(self.motor_speeds)
 
+
 class MotorControlBench:
+    """Piece of code that can be run as an individual controller, or easily called in a test bench
+
+    In it, we launch the necessary publishers and subscribers, and of course, the controller itself.
+    We provide a step() function so the caller can conveniently controls when to issue a PWM command
+    """
+
     def __init__(self, left_pid_params: PIDParams, right_pid_params: PIDParams) -> None:
-        self.pid_controller = IncrementalPIDController(left_pid_params, right_pid_params) 
+        self.pid_controller = IncrementalPIDController(
+            left_pid_params, right_pid_params
+        )
         self.encoder_status_sub = SharedMemorySub(
             topic=rospy.get_param("/SHM_TOPIC/WHEEL_VELOCITIES"),
-            data_type = float,
-            arr_size = 2,
+            data_type=float,
+            arr_size=2,
             read_frequency=rospy.get_param("/PARAMS/ENCODER_PUB_FREQUENCY"),
-            callback = self.pid_controller.store_speed,
-            start_connection_callback = self.pid_controller.start_getting_wheel_vel_updates,
-            no_connection_callback = self.pid_controller.stop_getting_wheel_vel_updates,
-            debug = False
+            callback=self.pid_controller.store_speed,
+            start_connection_callback=self.pid_controller.start_getting_wheel_vel_updates,
+            no_connection_callback=self.pid_controller.stop_getting_wheel_vel_updates,
+            debug=False,
         )
 
         self.commanded_wheel_vel_sub = SharedMemorySub(
@@ -110,29 +158,42 @@ class MotorControlBench:
             callback=self.pid_controller.store_commanded_speed,
             start_connection_callback=self.pid_controller.start_taking_commands,
             no_connection_callback=self.pid_controller.stop_taking_commands,
-            debug=False
+            debug=False,
         )
 
         # This takes in pwm in [0,1]
         self.motor_commands_pub = SharedMemoryPub(
             topic=rospy.get_param("/SHM_TOPIC/MOTOR_COMMANDS"),
-            data_type = float,
-            arr_size = 2,
-            debug = False 
+            data_type=float,
+            arr_size=2,
+            debug=False,
         )
         self.rate = Rate(rospy.get_param("/PARAMS/MOTOR_PUB_FREQUENCY"))
 
+    def get_actual_speeds(self) -> Tuple[float, float]:
+        """Convenient function for the user to get the current motor speeds
+
+        Returns:
+            Tuple[float, float]: current motor speeds, sp the
+        """
+        return self.pid_controller.get_actual_speeds()
+
     def step(self):
+        """calculates PWM based on the current motor speeds, and control goal, then publish it.
+
+        Note that publishing the PWM is how we let the motor driver know about our control goal
+        Also, we are sleeping according to the rate.
+        """
         pwm = self.pid_controller.get_pwms()
         self.motor_commands_pub.publish(list(pwm))
         self.rate.sleep()
-        return self.pid_controller.get_actual_speeds()
-        
+
+
 if __name__ == "__main__":
-    # How to test: 1. store primary speed as TODO; GETTING_COMMANDED_VEL = True 
+    # How to test:
     # 2. add print statement for motor speed as TODO
 
     rospy.init_node("motor_controller")
-    mcb = MotorControlBench(PIDParams(1,0.3,0.0), PIDParams(1,0,0))
+    mcb = MotorControlBench(PIDParams(1, 0.3, 0.0), PIDParams(1, 0, 0))
     while not rospy.is_shutdown():
         mcb.step()
