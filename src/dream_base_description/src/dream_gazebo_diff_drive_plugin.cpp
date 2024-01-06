ca8eb39c36e6b14bb156df52b8b4bb3b9633fbf8
ricocoyeah <rjia@diligentrobots.com>
2024-01-06 17:08:55 -0600

    diff drive works but needs some cleaning

--- src/dream_base_description/src/dream_gazebo_diff_drive_plugin.cpp
+++ src/dream_base_description/src/dream_gazebo_diff_drive_plugin.cpp
@@ -3,8 +3,8 @@
  * https://github.com/ros-simulation/gazebo_ros_pkgs/blob/noetic-devel/gazebo_plugins/src/gazebo_ros_diff_drive.cpp
  * What this plugin does:
  *  - Set joint vel instataneously
-    - perfect odom: base_link pose, vel;  for testing
-    - publishWheelJointState: perfect encoder, 
+    - Publish perfect odom as /tf: base_link pose, vel;  for testing
+    - Publish wheel position perfectly in m/s: 
  * Gazebo Notes: 
     - a plugin is a shared lib and inserted into simulation. Has direct access to all functionalities in Gazebo
         - Overview: https://classic.gazebosim.org/tutorials?tut=plugins_hello_world
@@ -34,6 +34,9 @@
 #include <gazebo_plugins/gazebo_ros_utils.h>
 #include <gazebo/physics/physics.hh>
 #include <nav_msgs/Odometry.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
 
 #include <ros/ros.h>
 #include "ros/callback_queue.h"
@@ -62,7 +65,6 @@ namespace gazebo
     class DreamGazeboDiffDrivePlugin: public ModelPlugin{
         private: ros::Subscriber wheel_vel_cmd_sub_;
         private: std::unique_ptr<ros::NodeHandle> nh_;
-        private: ros::Publisher odom_pub_;
         private: ros::Publisher wheel_joint_states_pub_;
         // A ROS callbackqueue that helps process messages
         private: ros::CallbackQueue node_callback_queue_;
@@ -70,8 +72,12 @@ namespace gazebo
         private: std::vector<physics::JointPtr> wheel_joints_;
         private: std::array<double, 2> wheel_vel_cmd_ = {0.0, 0.0};
         private: event::ConnectionPtr update_connection_;
-        private: physics::ModelPtr model;
+        private: physics::ModelPtr model_;
         private: double WHEEL_RADIUS_;
+        private: tf2_ros::TransformBroadcaster br_;
+        private: geometry_msgs::TransformStamped groud_truth_odom_;
+        private: double publish_period_;
+        private: common::Time last_update_time_;
 
         public: DreamGazeboDiffDrivePlugin(){
         }
@@ -90,11 +96,11 @@ namespace gazebo
             /********************** Get the joints params **********************/
             ROS_INFO("Loading wheel params for DreamGazeboDiffDrivePlugin");
             std::string left_wheel_joint, right_wheel_joint;
-            find_element<std::string>("left_wheel_joint", left_wheel_joint, _sdf);
-            find_element<std::string>("right_wheel_joint", right_wheel_joint, _sdf);
+            find_element("left_wheel_joint", left_wheel_joint, _sdf);
+            find_element("right_wheel_joint", right_wheel_joint, _sdf);
             find_element<double>("wheel_radius", WHEEL_RADIUS_, _sdf);
             wheel_joints_.resize(2);
-            this->model = _parent;
+            model_ = _parent;
             wheel_joints_[LEFT] = _parent->GetJoint(left_wheel_joint);
             wheel_joints_[RIGHT] = _parent->GetJoint(right_wheel_joint);
             wheel_joints_[LEFT]->SetParam("fmax", 0, 10000.0);
@@ -106,7 +112,7 @@ namespace gazebo
             this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
                 std::bind(&DreamGazeboDiffDrivePlugin::OnUpdate, this));
             std::string wheel_vel_topic;
-            find_element<std::string>("commanded_wheel_vel_topic", wheel_vel_topic, _sdf);
+            find_element("commanded_wheel_vel_topic", wheel_vel_topic, _sdf);
             // Create a named topic, and subscribe to it.
             ros::SubscribeOptions so =
             ros::SubscribeOptions::create<std_msgs::Float32MultiArray>(
@@ -119,11 +125,21 @@ namespace gazebo
             this->node_callback_queue_thread_ = std::thread(std::bind(&DreamGazeboDiffDrivePlugin::node_callback_queue_thread_func, this));
 
             // Spin up publishers for odom and wheel joint states
-            std::string odom_topic, wheel_joint_states_topic;
-            find_element<std::string>("odom_topic", odom_topic, _sdf);
-            find_element("wheel_joint_states_topic", wheel_joint_states_topic, _sdf);
-            odom_pub_ = nh_->advertise<nav_msgs::Odometry>(odom_topic, 1);
-            wheel_joint_states_pub_ = nh_->advertise<std_msgs::Float32MultiArray>(wheel_joint_states_topic, 1);
+            std::string wheel_joint_pos_topic;
+            find_element("wheel_joint_pos_topic", wheel_joint_pos_topic, _sdf);
+            wheel_joint_states_pub_ = nh_->advertise<std_msgs::Float32MultiArray>(wheel_joint_pos_topic, 1);
+            
+            std::string map_frame_name, base_link_frame_name;
+            find_element("map_frame_name", map_frame_name, _sdf);
+            find_element("base_link_frame_name", base_link_frame_name, _sdf);
+            groud_truth_odom_.header.frame_id = map_frame_name;
+            groud_truth_odom_.child_frame_id = base_link_frame_name;
+
+            // encoder publisher
+            double encoder_pub_frequency;
+            find_element("encoder_pub_frequency", encoder_pub_frequency, _sdf);
+            publish_period_ = 1.0/encoder_pub_frequency;
+            last_update_time_ = model_->GetWorld()->SimTime();
 
             ROS_INFO("DreamGazeboDiffDrivePlugin loaded.");
         }
@@ -134,6 +150,34 @@ namespace gazebo
             // setting joint velocities in rad/s
             wheel_joints_[LEFT]->SetParam("vel", 0, wheel_vel_cmd_[LEFT]);
             wheel_joints_[RIGHT]->SetParam("vel", 0, wheel_vel_cmd_[RIGHT]);
+
+            auto current_time = model_->GetWorld()->SimTime();
+            double seconds_since_last_update = (current_time - last_update_time_).Double();
+            if ( seconds_since_last_update > publish_period_){
+                last_update_time_ = current_time;   
+                // wheel vel published in m/s
+                ros::Time current_time = ros::Time::now();
+                auto wheel_joint_msg = std_msgs::Float32MultiArray();
+                for (unsigned int i = 0; i < wheel_joints_.size(); i++){
+                    wheel_joint_msg.data.push_back(wheel_joints_[i]->Position(0) * WHEEL_RADIUS_);
+                }
+                wheel_joint_states_pub_.publish(wheel_joint_msg);
+                // publish odomground_truth
+                ignition::math::Pose3d odom = model_->WorldPose();
+                auto quat = tf2::Quaternion();
+                quat.setRPY(odom.Rot().Roll(), odom.Rot().Pitch(), odom.Rot().Yaw());
+                groud_truth_odom_.transform.translation.x = odom.Pos().X();
+                groud_truth_odom_.transform.translation.y = odom.Pos().Y();
+                groud_truth_odom_.transform.translation.z = odom.Pos().Z();
+                groud_truth_odom_.transform.rotation.x = quat.x();
+                groud_truth_odom_.transform.rotation.y = quat.y();
+                groud_truth_odom_.transform.rotation.z = quat.z();
+                groud_truth_odom_.transform.rotation.w = quat.w();
+                groud_truth_odom_.header.stamp = current_time;
+                // TODO: not publishing twist for now
+                br_.sendTransform(groud_truth_odom_);
+            }
+
         }
 
         private: void wheel_vel_cmd_sub_cb(const std_msgs::Float32MultiArrayConstPtr& msg){
