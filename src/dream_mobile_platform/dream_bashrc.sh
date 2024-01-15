6fb735b8ae82f6524a994477410cce298efb919b
ricocoyeah <rjia@diligentrobots.com>
2024-01-14 23:06:38 -0600

    bash

--- src/dream_mobile_platform/dream_bashrc.sh
+++ src/dream_mobile_platform/dream_bashrc.sh
@@ -1,6 +1,8 @@
 # Contents of ros_env_setup.sh
 sudo_ros_preserve_env(){
+    # when passing args from one func to another, use $@ expansion
     local cmd="$@"
+    # -E preserves all current user's env variables.
     sudo -E /bin/bash -c "source ${WORKDIRECTORY}/devel/setup.bash; $cmd"
 }
 
