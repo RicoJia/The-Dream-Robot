4b3bd7ae6ad657574b712af48abc134f8f484c05
ricocoyeah <rjia@diligentrobots.com>
2024-01-29 09:51:50 -0600

    dream gmapping with basic test infra

---
+++ src/dream_gmapping/include/dream_gmapping/dream_gmapping.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <memory>
+#include <ros/node_handle.h>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/message_filter.h>
+#include <message_filters/subscriber.h>
+#include <memory>
+#include <geometry_msgs/TransformStamped.h>
+
+namespace DreamGMapping{
+    class DreamGMapper{
+        public:
+            DreamGMapper(ros::NodeHandle& nh_);
+            ~DreamGMapper();
+            // this signature is required for scan_filter_ptr_
+            void laser_scan(const boost::shared_ptr<const sensor_msgs::LaserScan>& scan_msg);
+        protected:
+            std::string base_frame_="base_link";
+            std::string map_frame_="map"; 
+            std::string odom_frame_="odom";
+            double map_update_interval_=0.1; // 10 hz
+
+            bool received_first_laser_scan_ = false;
+            tf2_ros::Buffer tf_buffer_ = tf2_ros::Buffer();
+            tf2_ros::TransformListener tf_listener_ = tf2_ros::TransformListener(tf_buffer_);
+            std::unique_ptr<message_filters::Subscriber<sensor_msgs::LaserScan>> scan_sub_ptr_;
+            std::unique_ptr<tf2_ros::MessageFilter<sensor_msgs::LaserScan>> scan_filter_ptr_;
+    };
+}
--- src/dream_gmapping/src/dream_gmapping.cpp
+++ src/dream_gmapping/src/dream_gmapping.cpp
@@ -1,7 +1,75 @@
-#include "simple_robotics_cpp_utils/math_utils.hpp"
-#include <iostream>
+/** @file 
+    @brief Rao-Blackwellized Gmapping, with the counting mapping model
+    @author Rico Ruotong Jia
+    @date 2024-01-28
+    SUBSCRIBES:
+    - @subscribes: /scan: laser scan data  
+    - @subscribes: /tf: listens for for odom -> baselink
+    PUBLISHES:
+    - @publishes: /map: map of the current environment
+    - @publishes: /tf: map -> odom transform
 
-int main(){
-    auto result = SimpleRoboticsCppUtils::draw_from_pdf_normal(1.0, 1.0);
-    std::cout << result << std::endl;
+    PARAMETERS:
+    - @param: base_frame
+    - @param: map_frame
+    - @param: odom_frame
+    - @param: map_update_interval
+    laser params
+    - @param: maxRange: maximum laser scanner range
+    - @param: sigma: standard deviation for the scan matching process 
+    - @param: search window for scan matching
+    // TODO: scan matching kernel, and steps?
+    // TODO: benefits of 
+        scan_filter_sub_ = new message_filters::Subscriber<sensor_msgs::LaserScan>(node_, "scan", 5);
+        scan_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(*scan_filter_sub_, tf_, odom_frame_, 5);
+        scan_filter_->registerCallback(boost::bind(&SlamGMapping::laserCallback, this, _1));
+*/
+
+
+// Need message filter. Also, a 1s "expiration time"?
+#include "simple_robotics_cpp_utils/math_utils.hpp"
+#include "dream_gmapping/dream_gmapping.hpp"
+#include <iostream>
+#include <memory>
+#include <ros/node_handle.h>
+#include <ros/ros.h>
+namespace DreamGMapping
+{
+    DreamGMapper::DreamGMapper(ros::NodeHandle& nh_)
+    {
+        // Read parameters without default values here, so they must be specified in launch file
+        nh_.getParam("base_frame", base_frame_);
+        nh_.getParam("map_frame", map_frame_);
+        nh_.getParam("odom_frame", odom_frame_);
+        nh_.getParam("map_update_interval", map_update_interval_);
+        ROS_INFO_STREAM("Successfully read parameters for dream_gmapping");
+
+        scan_sub_ptr_ = std::make_unique<message_filters::Subscriber<sensor_msgs::LaserScan>>(nh_, "scan", 1);
+        // wait on the odom frame
+        scan_filter_ptr_ = std::make_unique<tf2_ros::MessageFilter<sensor_msgs::LaserScan>>(*scan_sub_ptr_, tf_buffer_, odom_frame_, 1, nh_);
+        scan_filter_ptr_ -> registerCallback(boost::bind(&DreamGMapper::laser_scan, this, _1));
+    }
+
+    DreamGMapper::~DreamGMapper() = default;
+
+    void DreamGMapper::laser_scan(const boost::shared_ptr<const sensor_msgs::LaserScan>& scan_msg){
+        // TODO: test the filter, with a laserscan msg first, then with odom
+        if (!received_first_laser_scan_){
+            // Store the laser->base transform
+            received_first_laser_scan_ = true;
+            ROS_DEBUG_STREAM("Received first laser scan");
+        }
+    }
+}
+int main(int argc, char**argv){
+    ros::init(argc, argv, "dream_gmapping");
+    ros::NodeHandle nh_("~");
+    DreamGMapping::DreamGMapper dg(nh_);
+
+    // init map
+    // get laser scanner -> base link tf from the scan topic
+
+    // For simplicity, we are using a single threaded model for subscribers
+    ros::spin();
+    return 0;
 }
